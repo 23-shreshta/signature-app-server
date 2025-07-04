38dd323b1c37124614b6c20463eedfe3988e884e
23-shreshta <dasshreshta@gmail.com>
2025-07-04 23:08:29 +0530

    Remove deprecated MongoDB connection options to fix warnings

--- server/config/db.js
+++ server/config/db.js
@@ -3,10 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     const MONGODB_URI = 'mongodb+srv://signpdf:before@cluster0.ub6ipqr.mongodb.net/signature-app?retryWrites=true&w=majority&appName=Cluster0';
-    const conn = await mongoose.connect(MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(MONGODB_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
