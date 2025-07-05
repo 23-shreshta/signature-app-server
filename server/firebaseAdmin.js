0083a91d96b1f42dc4ab8c0736fa0173fb6ab941
23-shreshta <dasshreshta@gmail.com>
2025-07-05 18:46:40 +0530

    Add Firebase token verification middleware

---
+++ server/firebaseAdmin.js
@@ -0,0 +1,10 @@
+// server/firebaseAdmin.js
+const admin = require('firebase-admin');
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.applicationDefault(), // or use a service account
+  });
+}
+
+module.exports = admin;
